5472ce21530ff62b26ec69b70f8e1d8f1c1e6c5d
Faiizuxd <faizuxd8@gmail.com>
2025-07-05 18:08:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, render_template_string, redirect import threading import requests import time
-
+from flask import Flask, request, render_template_string, redirect
+import threading
+import requests
+import time
 app = Flask(name) app.debug = True
 
 active_threads = {} thread_info = {}
